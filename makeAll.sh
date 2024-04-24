5d484cf270847a9a8a87e8b3bd410acf2d6fba0e
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-04-24 21:03:06 +0000

    #3 Make makeAll.sh single process

--- makeAll.sh
+++ makeAll.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-sh makeAstigmatismCont.sh &
-sh makeAstigmatismSpec.sh &
-sh makeBeammap.sh &
-sh makeFocusXY.sh &
-sh makeFocusZcont.sh &
-sh makeFocusZspec.sh &
-sh makePointingCont.sh &
-sh makePointingLine.sh &
-sh makeScinecePs.sh &
-sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v01.scr &
-sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v02.scr &
-sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v03.scr &
-sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v04.scr &
-sh makeAllFreqIDs.sh templates/b4r_SciencePs_Borisov_spec02_v01.scr &
-sh makeAllFreqIDs.sh templates/b4r_SciencePs_OriKL_spec02_v01.scr &
-sh makeAllFreqIDs.sh templates/b4r_FocusZ_OriKL_spec02_v01.scr &
-sh makeAllFreqIDs.sh templates/b4r_Astigmatism_OriKL_spec02_v01.scr &
+sh makeAstigmatismCont.sh
+sh makeAstigmatismSpec.sh
+sh makeBeammap.sh
+sh makeFocusXY.sh
+sh makeFocusZcont.sh
+sh makeFocusZspec.sh
+sh makePointingCont.sh
+sh makePointingLine.sh
+sh makeScinecePs.sh
+sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v01.scr
+sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v02.scr
+sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v03.scr
+sh makeAllFreqIDs.sh templates/b4r_PointingLine_OriKL_spec02_v04.scr
+sh makeAllFreqIDs.sh templates/b4r_SciencePs_Borisov_spec02_v01.scr
+sh makeAllFreqIDs.sh templates/b4r_SciencePs_OriKL_spec02_v01.scr
+sh makeAllFreqIDs.sh templates/b4r_FocusZ_OriKL_spec02_v01.scr
+sh makeAllFreqIDs.sh templates/b4r_Astigmatism_OriKL_spec02_v01.scr
